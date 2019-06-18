bae1686444ba1023c0d662d8775d88ee25a568c9
meddlin <drushing.dev@gmail.com>
2019-06-18 13:03:20 -0500

    Sample order data is displayed in the dashboard.

--- EcommerceShipping/back-office/src/Dashboard.js
+++ EcommerceShipping/back-office/src/Dashboard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { Order } from './_components/Order/Order';
+import orders from './order-data';
 
 export class Dashboard extends Component {
     
     render() {
+        const _orders = orders;
+
         return (
             <div>
                 <p>Dashboard</p>
+
+                <ul>
+                    {_orders.map(o => <li key={o.productId}>{o.productName}</li>)}
+                </ul>
 
                 <hr />
 
