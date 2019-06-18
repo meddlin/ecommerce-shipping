4f60ed8010b0e07923db06969c380f8c8b6b850a
meddlin <drushing.dev@gmail.com>
2019-06-18 00:02:54 -0500

    Created the basic models Product, Order, Holiday
    - Didn't implement ship date as that has a lot of work better defined in another branch
    - Holiday is optional, so it'll be fleshed out more later

---
+++ EcommerceShipping/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceShipping.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int InventoryQuantity { get; set; }
+        public bool ShipOnWeekends { get; set; }
+        public int MaxBusinessDaysToShip { get; set; }
+        public DateTime ShipDate { get; set; }
+    }
+}
